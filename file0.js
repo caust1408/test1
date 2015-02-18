d81582e937fc147758ef2326d7ef84856d70e8f0
Craig <cav9871@saintjoe.edu>
2015-02-18 13:38:54 -0500

    Just added my comments I forgot

--- file0.js
+++ file0.js
@@ -2,22 +2,30 @@
 // Craig Austgen
 // CMP237
 
+// creates variable x set to 1
 var x = 1;
+// creates variable craig set to false
 var craig = false;
+// if statement testing whether x is equal to 1
 if ( x == 1) {
+	// prints out value of x
 	print('x is ' + x);
 }
    else {
+	   
 	   print('Maybe next time');
    }
-
+// if x is less than 10 it will print out the number and increment it
 while ( x < 10) {
 	print('x equals ' + x);
 	x++;
 }
-
+// if craig is false then print false
 if ( craig == false) {
 	print('Craig is false');
+	// changes state of craig from false to true
+	craig = true;
+	print('Now craig is true ' + craig);
 }
 
 
