2cb73309f1c35c4719c9d0efd11f8af865f347db
Craig <cav9871@saintjoe.edu>
2015-02-18 13:40:15 -0500

    Added my comments I forgot

--- file1.js
+++ file1.js
@@ -2,8 +2,12 @@
 // file # 1
 //
 
+// creates variable cmp and sets it to 237
 var cmp = 237;
+// if cmp equals 237 then change cmp to 344
  if (cmp == 237) {
+	 // sets cmp to 344
 	 cmp = 344;
+	 // prints it out
 	 print('cmp has changed to ' + cmp);
  }
