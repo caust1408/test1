9e751496cd2ed0eb75aa0fbace7178c125d8f46e
Craig <cav9871@saintjoe.edu>
2015-02-18 12:25:22 -0500

    This is my first file for practicum

---
+++ file0.js
@@ -0,0 +1,25 @@
+// This is file 0
+// Craig Austgen
+// CMP237
+
+var x = 1;
+var craig = false;
+if ( x == 1) {
+	print('x is ' + x);
+}
+   else {
+	   print('Maybe next time');
+   }
+
+while ( x < 10) {
+	print('x equals ' + x);
+	x++;
+}
+
+if ( craig == false) {
+	print('Craig is false');
+}
+
+
+
+
