ec93a714c373c27c4d3a3d5bf3b6ce5b7fa7986e
Craig <cav9871@saintjoe.edu>
2015-02-18 13:42:36 -0500

    Added my comments I forgot about

--- file2.js
+++ file2.js
@@ -1,14 +1,18 @@
 // Craig Austgen
 // file 2 
 
-
+// declares a function with the variable name cat
 var cat = function() {
+	// all the function does it print out cat
 	print('cat');
 }
-
+// creates a declaritive function with one parameter
 function dog(treat) {
+	// prints parameter with strings
 	print('I want ' + treat + ' treats');
 }
 
-dog(5);
+dog(5);	// calls dog function
+
+// calls cat function
 cat();
